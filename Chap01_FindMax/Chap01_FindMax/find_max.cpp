266667e74ffdf277ff056e8fab458251514670e1
ynsk-yang <yus4342@naver.com>
2023-03-20 22:55:48 +0900

    chap01 project added

---
+++ Chap01_FindMax/Chap01_FindMax/find_max.cpp
@@ -0,0 +1,20 @@
+#include <cstdio>
+
+int findArrayMax(int score[], int n) //자료구조 : 배열 array
+{
+	int tmp = score[0];
+	for (int i = 1; i < n; i++) {
+		if (score[i] > tmp) {
+			tmp = score[i];
+		}
+	}
+	return tmp;
+}
+
+void main()
+{
+	int score[5] = { 12, 123, 12, 54, 1, };
+	int out;
+	out = findArrayMax(score, 5);
+	printf("result = %d", out);
+}
