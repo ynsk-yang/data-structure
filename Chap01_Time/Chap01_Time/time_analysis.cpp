f7d767f7a0db52b78e83cb3047c0d2ef7c0cc213
ynsk-yang <yus4342@naver.com>
2023-03-27 12:41:41 +0900

    Chap01_Time Update

--- Chap01_Time/Chap01_Time/time_analysis.cpp
+++ Chap01_Time/Chap01_Time/time_analysis.cpp
@@ -2,6 +2,20 @@
 #include <cstdlib>		// 표준 라이브러리 헤더파일
 #include <ctime>		// time과 관련된 헤더파일
 
+int sum_of_N(int n)
+{
+	int sum = 0;
+	for (int i = 0; i < n + 1; i++) {
+		sum = sum + 1;
+	}
+	return sum;
+}
+
+int sum_of_N2(int n)
+{
+	return (n * (n + 1)) / 2;
+}
+
 int main()
 {
 	clock_t start, finish;	// 시작 시간, 끝나는 시각
@@ -9,7 +23,11 @@ int main()
 
 	start = clock();		// 현재 시각을 start에 저장
 	//실행시간을 측정하고자하는 코드 입력
-	int a = 10 + 20;
+	// 1. sum_of_N 함수 호출
+	//int sum = sum_of_N(100000000);
+
+	// 2. sum_of_N2 함수 호출
+	int sum = sum_of_N2(100000000);
 	// ..
 	finish = clock();		//현재 시각을 finish에 저장
 	duration = (double)(finish - start) / CLOCKS_PER_SEC;	//(끝난시간 - 시작시간) = 코드 실행에 걸린 시간
