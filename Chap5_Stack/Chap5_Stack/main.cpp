b9961c7439bdae61b2b76f8559171b272d4c12ba
ynsk-yang <yus4342@naver.com>
2023-05-13 00:48:00 +0900

    chap5 stack update

---
+++ Chap5_Stack/Chap5_Stack/main.cpp
@@ -0,0 +1,22 @@
+#include "ArrayStack.h"
+
+int main()
+{
+	// ArrayStack 클래스로 stack 객체 생성 = stack은 ArrayStack의 인스턴스
+	ArrayStack stack;
+	for (int i = 1; i < 10; i++)
+		stack.push(i);
+
+
+	stack.display();
+	stack.pop();
+	stack.pop();
+	stack.pop();
+	stack.pop();
+	stack.pop();
+	stack.display();
+	stack.peek();
+	stack.display();
+	
+	return 0;
+}
